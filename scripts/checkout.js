1dc184615824811ad89a389d22ba467500b4ba99
marcohany <marcohany195@gmail.com>
2024-12-28 21:47:44 +0200

    delete products from html page by clicking delete link

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -11,7 +11,9 @@ cart.forEach((cartItem) => {
     if (product.id === productId) {
       matchingProduct = product
       cartSummaryHtml += `
-     <div class="cart-item-container">
+     <div class="cart-item-container js-cart-item-container-${
+       matchingProduct.id
+     }">
             <div class="delivery-date">Delivery date: Tuesday, June 21</div>
 
             <div class="cart-item-details-grid">
@@ -92,6 +94,9 @@ document.querySelectorAll('.js-delete-quantity-link').forEach((link) => {
   link.addEventListener('click', () => {
     const productId = link.dataset.productId
     removeFromCart(productId)
-    console.log(cart)
+    const container = document.querySelector(
+      `.js-cart-item-container-${productId}`
+    )
+    container.remove()
   })
 })
